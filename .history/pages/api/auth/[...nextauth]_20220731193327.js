a1772c6464bff7fdaa9573fd2604ff90ac70874a
Sehaj bindra <sehajbindra1234@gmail.com>
2022-08-04 10:46:36 +0530

    next auth

--- .history/pages/api/auth/[...nextauth]_20220731193327.js
+++ .history/pages/api/auth/[...nextauth]_20220731193327.js
@@ -18,31 +18,7 @@ export default NextAuth({
     // ...add more providers here
   ],
 
-  secret: process.env.NEXT_AUTH_SECRET,
-  secret: process.env.NEXT_AUTH_JWT_SECRET,
-
   pages: {
     signIn: "/login",
-  },
-
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.user.username = session.user.name
-        .split("")
-        .join("")
-        .toLowerCase();
-      // Sehaj Bindra to sehajbindra
-
-      session.user.uid = token.sub;
-
-      return session;
-    },
   },
 });
--- .history/pages/api/auth/[...nextauth]_20220804104428.js
+++ .history/pages/api/auth/[...nextauth]_20220804104428.js
@@ -24,25 +24,4 @@ export default NextAuth({
   pages: {
     signIn: "/login",
   },
-
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      session.user.username = session.user.name
-        .split("")
-        .join("")
-        .toLowerCase();
-      // Sehaj Bindra to sehajbindra
-
-      session.user.uid = token.sub;
-
-      return session;
-    },
-  },
 });
